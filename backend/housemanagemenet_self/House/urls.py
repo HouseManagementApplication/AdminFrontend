03b823dc2f18d995c378519c947a2ab376a80ac7
shivaprasadGowd <shivaprasadgowda9380@gmail.com>
2026-09-02 14:52:33 +0530

    frontend updated

--- backend/housemanagemenet_self/House/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import HouseAPI
-
-
-urlpatterns =[
-    path("house/", HouseAPI.as_view(),name="House"),
-]
--- backend/practice/Myapp/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from.views import Loginview
-
-urlpatterns =[
-    path("login/",Loginview.as_view(),name ="login"),
-]
